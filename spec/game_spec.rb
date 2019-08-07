ed22f58153acf053305992b3f23c1b3ebd0a7d25
ViriCruz <viridiana.cruz.olivera@gmail.com>
2019-08-07 10:14:41 -0700

    more examples added to test if the won? method trigger correctly everytime it checks if a player won the match

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -10,6 +10,36 @@ describe Game do
         game.board.update_board('X',3)
         expect( game.won?("X") ).to eql(true)
       end
+
+      it 'returns false for O player' do
+        expect( game.won?("O") ).to eql(false)
+      end
+    end
+
+    context 'when O O O in diagonal' do
+      it 'returns true' do
+        game.board.update_board('O',3)
+        game.board.update_board('O',5)
+        game.board.update_board('O',7)
+        expect( game.won?("O") ).to eql(true)
+      end
+
+      it 'returns false for X player' do
+        expect( game.won?("X") ).to eql(false)
+      end
+    end
+
+    context 'when X X X in vertical line' do
+      it 'returns true' do
+        game.board.update_board('X',1)
+        game.board.update_board('X',4)
+        game.board.update_board('X',7)
+        expect( game.won?('X') ).to eql(true)
+      end
+
+      it 'returns false' do
+        expect( game.won?("O") ).to eql(false)
+      end
     end
   end
 end
